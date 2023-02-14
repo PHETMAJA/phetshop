8c39fc40cdb21ddd526632fcaa688c1ce66a7166
PHETMAJA <Thanachot698@gmail.com>
2023-02-14 09:59:40 +0700

    add-routing

--- README.md
+++ README.md
@@ -11,3 +11,8 @@
    5.2 add product images
    5.3 render products
    5.4 style products
+
+6. Add routing
+   6.1 npm i react-routing-dom
+   6.2 create route for home screen
+   6.3 create route for product screen
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,33 +1,22 @@
-import data from './data';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import HomeScreen from './screen/HomeScreen';
+import ProductScreen from './screen/ProductScreen';
 
 function App() {
   return (
-    <div>
-      <header>
-        <a href="/">phetshop</a>
-      </header>
-      <main>
-        <h1>Featured Products</h1>
-        <div className="products">
-          {data.products.map((product) => (
-            <div key={product.slug} className="product">
-              <a href={`/product/${product.slug}`}>
-                <img src={product.image} alt={product.name} />
-              </a>
-              <div className="product-info">
-                <a href={`/product/${product.slug}`}>
-                  <p>{product.name}</p>
-                </a>
-                <p>
-                  <strong>${product.price}</strong>
-                </p>
-                <button>Add to cart</button>
-              </div>
-            </div>
-          ))}
-        </div>
-      </main>
-    </div>
+    <BrowserRouter>
+      <div>
+        <header>
+          <a href="/">PHETSHOP</a>
+        </header>
+        <main>
+          <Routes>
+            <Route path="/product/:slug" element={<ProductScreen />} />
+            <Route path="/" element={<HomeScreen />} />
+          </Routes>
+        </main>
+      </div>
+    </BrowserRouter>
   );
 }
 
